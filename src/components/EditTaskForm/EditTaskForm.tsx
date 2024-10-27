ebd051538d1b20c9d25a203ccd7a89966ed78102
h-yabi2 <hur4k0dt@gmail.com>
2024-10-27 17:53:03 +0900

    Add form submission handling using useFormState hook

--- src/components/EditTaskForm/EditTaskForm.tsx
+++ src/components/EditTaskForm/EditTaskForm.tsx
@@ -2,7 +2,8 @@
 
 import React, { useState } from "react";
 import { TaskDocument } from "@/models/task";
-import { updateTask } from "@/actions/task";
+import { updateTask, FormState } from "@/actions/task";
+import { useFormState, useFormStatus } from "react-dom";
 
 interface EditTaskFormProps {
   task: TaskDocument;
@@ -14,9 +15,29 @@ const EditTaskForm: React.FC<EditTaskFormProps> = ({ task }) => {
   const [dueDate, setDueDate] = useState(task.dueDate);
   const [isCompleted, setIsCompleted] = useState(task.isCompleted);
 
+  const updateTaskWithId = updateTask.bind(null, task._id);
+  const initialState: FormState = {
+    error: "",
+  };
+  const [state, formAction] = useFormState(updateTaskWithId, initialState);
+
+  const SubmitButton = () => {
+    const { pending } = useFormStatus();
+    return (
+      <button
+        type="submit"
+        className="mt-8 py-2 w-full rounded-md text-white 
+        bg-gray-800 hover:bg-gray-700 text-sm font-semibold shadow-sm disabled:opacity-50"
+        disabled={pending}
+      >
+        Edit
+      </button>
+    );
+  };
+
   return (
     <div className="mt-10 mx-auto w-full max-w-sm">
-      <form action="">
+      <form action={formAction}>
         <div>
           <label htmlFor="title" className="block text-sm font-medium">
             タイトル
@@ -77,13 +98,10 @@ const EditTaskForm: React.FC<EditTaskFormProps> = ({ task }) => {
             タスクを完了にする
           </label>
         </div>
-        <button
-          type="submit"
-          className="mt-8 py-2 w-full rounded-md text-white 
-        bg-gray-800 hover:bg-gray-700 text-sm font-semibold shadow-sm"
-        >
-          Edit
-        </button>
+        <SubmitButton />
+        {state.error && (
+          <p className="mt-4 text-sm text-red-500">{state.error}</p>
+        )}
       </form>
     </div>
   );
